f92f24264cb0b769a898842c17c6a174154b220b
Jonathan Ballet <jon@multani.info>
2023-08-28 23:16:57 +0200

    Better linter configuration

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func newZapConfig() zap.Config {
 	// Log normal logs to stdout, error logs to stderr
 	config.OutputPaths = []string{"stdout"}
 	config.ErrorOutputPaths = []string{"stderr"}
+
 	return config
 }
 
@@ -38,50 +39,58 @@ func main() {
 	flag.Parse()
 
 	zapConfig := newZapConfig()
+
 	logger, err := zapConfig.Build()
 	if err != nil {
 		panic(err)
 	}
 
-	h := httpHandler{
+	handler := httpHandler{
 		Logger: logger,
 	}
 
 	router := mux.NewRouter()
-	router.HandleFunc("/alerts", h.alertHandler).Methods("POST")
-	router.HandleFunc("/ready", h.readyHandler).Methods("GET")
+	router.HandleFunc("/alerts", handler.alertHandler).Methods("POST")
+	router.HandleFunc("/ready", handler.readyHandler).Methods("GET")
+
+	var (
+		writeTimeout = 5 * time.Second
+		readTimeout  = 15 * time.Second
+	)
 
 	srv := &http.Server{
 		Handler: router,
 		Addr:    *address,
 
-		WriteTimeout: 5 * time.Second,
-		ReadTimeout:  15 * time.Second,
+		WriteTimeout: writeTimeout,
+		ReadTimeout:  readTimeout,
 	}
 
 	logger.Debug("Starting webhook receiver")
 	errorLog.Fatal(srv.ListenAndServe())
 }
 
-func (h *httpHandler) readyHandler(w http.ResponseWriter, r *http.Request) {
+func (h *httpHandler) readyHandler(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func (h *httpHandler) alertHandler(w http.ResponseWriter, r *http.Request) {
+func (h *httpHandler) alertHandler(writer http.ResponseWriter, r *http.Request) {
 	var alerts template.Data
 
 	// Check the payload is actually an Alertmanager payload.
 	err := json.NewDecoder(r.Body).Decode(&alerts)
 	if err != nil {
 		h.Logger.Error(fmt.Errorf("cannot parse request: %w", err).Error())
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+
 		return
 	}
 
 	output, err := json.Marshal(alerts)
 	if err != nil {
 		h.Logger.Error(fmt.Errorf("cannot serialize back alerts: %w", err).Error())
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+
 		return
 	}
 
@@ -91,14 +100,17 @@ func (h *httpHandler) alertHandler(w http.ResponseWriter, r *http.Request) {
 
 	var log func(msg string, fields ...zapcore.Field)
 
-	if alerts.Status == "firing" {
+	switch alerts.Status {
+	case "firing":
 		log = logger.Warn
-	} else if alerts.Status == "resolved" {
+
+	case "resolved":
 		log = logger.Info
-	} else {
+
+	default:
 		log = logger.Error
 	}
 
 	log("Events received")
-	w.WriteHeader(http.StatusOK)
+	writer.WriteHeader(http.StatusOK)
 }
